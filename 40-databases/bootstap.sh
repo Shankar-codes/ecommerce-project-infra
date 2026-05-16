3d07d1d84e998ecb7edf67814f6226ec48f98d6a
Shankar-codes <harikainteriors@gmail.com>
2026-05-16 18:45:09 +0530

    databases

--- 40-databases/bootstap.sh
+++ 40-databases/bootstap.sh
@@ -1,4 +1,5 @@
 #/bin/bash
 component=$1
 dnf install ansible -y
+pip3 install boto3 botocore
 ansible-pull -U https://github.com/Shankar-codes/ansible-roles-terraform.git -e component=$component main.yaml
