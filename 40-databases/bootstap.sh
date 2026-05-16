9d42957598c37ef2146db678e2a2288be38e9eca
Shankar-codes <harikainteriors@gmail.com>
2026-05-16 17:01:37 +0530

    databases

--- 40-databases/bootstap.sh
+++ 40-databases/bootstap.sh
@@ -1,3 +1,3 @@
 #/bin/bash
 dnf install ansible -y
-ansible-pull -u https://github.com/Shankar-codes/ansible-roles-terraform.git -e component=mongodb main.yaml
+ansible-pull -U https://github.com/Shankar-codes/ansible-roles-terraform.git -e component=mongodb main.yaml
