c1f60228675c0f6e505cd7bbc42e2586a8584236
Shankar-codes <harikainteriors@gmail.com>
2026-05-16 15:34:23 +0530

    databases

---
+++ 40-databases/main.tf
@@ -0,0 +1,31 @@
+resource "aws_instance" "mongodb" {
+  ami = local.ami_id
+  instance_type = var.instance_type
+  vpc_security_group_ids = [local.mongodb_sg_id]
+  subnet_id = local.database_subnet_ids
+  tags =merge(local.common_tags, {
+      Name = "${var.project_name}-${var.environment}-MongoDB"
+      Terraform = "true"
+  }
+  )
+}
+
+#this is null resource
+resource "terraform_data" "mongodb" {
+  triggers_replace = [
+    aws_instance.mongodb.id
+  ]
+
+connection {
+    type = "ssh"
+    host = "aws_instance.mongodb.private_ip"
+    user = "ec2-user"
+    password = "DevOps321"
+}
+
+provisioner "remote-exec" {
+    inline = [
+      "echo Hello world"
+    ]
+  }
+}
