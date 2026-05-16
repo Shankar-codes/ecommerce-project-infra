2e3fb33eda23ce7da6f648ae46b52d4e0cb71173
Shankar-codes <harikainteriors@gmail.com>
2026-05-16 18:18:39 +0530

    databases

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -81,3 +81,45 @@ provisioner "remote-exec" {
 }
 
 
+# rabbitmq instance
+resource "aws_instance" "rabbitmq" {
+  ami = local.ami_id
+  instance_type = var.instance_type
+  vpc_security_group_ids = [local.rabbitmq_sg_id]
+  subnet_id = local.database_subnet_ids
+  tags =merge(local.common_tags, {
+      Name = "${var.project_name}-${var.environment}-rabbitmq"
+      Terraform = "true"
+  }
+  )
+}
+
+#this is null resource
+resource "terraform_data" "rabbitmq" {
+  triggers_replace = [
+    aws_instance.rabbitmq.id
+  ]
+
+
+connection {
+    type = "ssh"
+    user = "ec2-user"
+    password = "DevOps321"
+    host = aws_instance.rabbitmq.private_ip
+}
+
+# Provisioner to copy the file - terraform copies the file to the ec2 instance
+provisioner "file" {
+  source      = "bootstap.sh"       # Local file path
+  destination = "/tmp/bootstap.sh"      # Remote path on EC2
+}
+
+provisioner "remote-exec" {
+    inline = [
+      "sudo chmod +x /tmp/bootstap.sh",
+      "sudo sh /tmp/bootstap.sh rabbitmq"
+    ]
+  }
+}
+
+
