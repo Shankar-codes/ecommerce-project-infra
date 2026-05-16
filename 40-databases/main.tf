dc5ca3f35850557cd877338b60421eeac07b3f76
Shankar-codes <harikainteriors@gmail.com>
2026-05-16 17:39:27 +0530

    databases

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -1,3 +1,4 @@
+# mongodb instance
 resource "aws_instance" "mongodb" {
   ami = local.ami_id
   instance_type = var.instance_type
@@ -33,7 +34,50 @@ provisioner "file" {
 provisioner "remote-exec" {
     inline = [
       "sudo chmod +x /tmp/bootstap.sh",
-      "sudo sh /tmp/bootstap.sh"
+      "sudo sh /tmp/bootstap.sh mongodb"
     ]
   }
 }
+
+# redis instance
+resource "aws_instance" "redis" {
+  ami = local.ami_id
+  instance_type = var.instance_type
+  vpc_security_group_ids = [local.redis_sg_id]
+  subnet_id = local.database_subnet_ids
+  tags =merge(local.common_tags, {
+      Name = "${var.project_name}-${var.environment}-redis"
+      Terraform = "true"
+  }
+  )
+}
+
+#this is null resource
+resource "terraform_data" "redis" {
+  triggers_replace = [
+    aws_instance.redis.id
+  ]
+
+
+connection {
+    type = "ssh"
+    user = "ec2-user"
+    password = "DevOps321"
+    host = aws_instance.redis.private_ip
+}
+
+# Provisioner to copy the file - terraform copies the file to the ec2 instance
+provisioner "file" {
+  source      = "bootstap.sh"       # Local file path
+  destination = "/tmp/bootstap.sh"      # Remote path on EC2
+}
+
+provisioner "remote-exec" {
+    inline = [
+      "sudo chmod +x /tmp/bootstap.sh",
+      "sudo sh /tmp/bootstap.sh redis"
+    ]
+  }
+}
+
+
