0fc7f2a86887b8443b14b5067b6eb67ef41d6df2
Shankar-codes <harikainteriors@gmail.com>
2026-05-16 15:53:59 +0530

    security group rules

--- 30-sg-rules/main.tf
+++ 30-sg-rules/main.tf
@@ -15,3 +15,13 @@ resource "aws_security_group_rule" "mongodb_bastion" {
   protocol          = "tcp"
   to_port           = 22
 }
+
+#this is attached to bastion sg to allow ssh access from laptop to bastion host
+resource "aws_security_group_rule" "bastion_laptop" {
+  type              = "ingress"
+  security_group_id = local.bastion_sg_id
+  cidr_blocks = ["0.0.0.0/0"]
+  from_port         = 22
+  protocol          = "tcp"
+  to_port           = 22
+}
