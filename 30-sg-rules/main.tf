6b7974929c060f3795e0f7fb4de22f19115a45fa
Shankar-codes <harikainteriors@gmail.com>
2026-05-16 17:39:02 +0530

    security group rules

--- 30-sg-rules/main.tf
+++ 30-sg-rules/main.tf
@@ -17,6 +17,36 @@ resource "aws_security_group_rule" "mongodb_bastion" {
   to_port           = 22
 }
 
+#bastion host connecting to redis server on port 22
+resource "aws_security_group_rule" "redis_bastion" {
+  type              = "ingress"
+  security_group_id = local.redis_sg_id # 
+  source_security_group_id = local.bastion_sg_id #
+  from_port         = 22
+  protocol          = "tcp"
+  to_port           = 22
+}
+
+#bastion host connecting to rabbitmq server on port 22
+resource "aws_security_group_rule" "rabbitmq_bastion" {
+  type              = "ingress"
+  security_group_id = local.rabbitmq_sg_id # 
+  source_security_group_id = local.bastion_sg_id #
+  from_port         = 22
+  protocol          = "tcp"
+  to_port           = 22
+}
+
+#bastion host connecting to mysql server on port 22
+resource "aws_security_group_rule" "mysql_bastion" {
+  type              = "ingress"
+  security_group_id = local.mysql_sg_id # 
+  source_security_group_id = local.bastion_sg_id #
+  from_port         = 22
+  protocol          = "tcp"
+  to_port           = 22
+}
+
 #this is attached to bastion sg to allow ssh access from laptop to bastion host
 resource "aws_security_group_rule" "bastion_laptop" {
   type              = "ingress"
