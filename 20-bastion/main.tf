d3ffe8eb4b5a554f62035d0665c62f5e5fcb31dd
Shankar-codes <harikainteriors@gmail.com>
2026-05-18 13:22:59 +0530

    bastion hostt

--- 20-bastion/main.tf
+++ 20-bastion/main.tf
@@ -3,9 +3,17 @@ resource "aws_instance" "bastion" {
   instance_type = var.instance_type
   vpc_security_group_ids = [local.bastion_sg_id]
   subnet_id = local.public_subnet_ids
+  user_data = file("bastion.sh")
+  iam_instance_profile = aws_iam_instance_profile.bastion.name
   tags =merge(local.common_tags, {
       Name = "${var.project_name}-${var.environment}-Bastion Host"
       Terraform = "true"
   }
   )
+
+  resource "aws_iam_instance_profile" "bastion" {
+  name = "baston"
+  role = "BastionAdminRole"
+}
+
 }
