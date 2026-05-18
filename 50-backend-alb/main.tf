99622523c83ea32a039abb7c1a3fc89fb170b5d1
Shankar-codes <harikainteriors@gmail.com>
2026-05-18 22:40:09 +0530

    backend

--- 50-backend-alb/main.tf
+++ 50-backend-alb/main.tf
@@ -28,3 +28,18 @@ resource "aws_lb_listener" "back_end" {
     }
   }
 }
+
+# creating R53 record for backend ALB backend-alb.ellamma.fun
+resource "aws_route53_record" "backend_alb" {
+  zone_id = var.zone_id
+  name    = "*.backend-alb.${var.domain_name}" #backend-alb.ellamma.fun
+  type    = "A"
+  
+  # these are details of aws alb
+  alias {
+    name                   = aws_lb.backend_alb.dns_name
+    zone_id                = aws_lb.backend_alb.zone_id
+    evaluate_target_health = true
+  }
+  
+}
