3caec68efe4b318479a5cb5fd0748748aa7e15a2
Shankar-codes <harikainteriors@gmail.com>
2026-05-18 17:00:16 +0530

    security group rules

--- 30-sg-rules/main.tf
+++ 30-sg-rules/main.tf
@@ -47,6 +47,27 @@ resource "aws_security_group_rule" "mysql_bastion" {
   to_port           = 22
 }
 
+#bastion host connecting to catalogue server on port 22
+resource "aws_security_group_rule" "catalogue_bastion" {
+  type              = "ingress"
+  security_group_id = local.catalogue_sg_id # 
+  source_security_group_id = local.bastion_sg_id #
+  from_port         = 22
+  protocol          = "tcp"
+  to_port           = 22
+}
+
+
+#bastion host connecting to catalogue server on port 22
+resource "aws_security_group_rule" "mongodb_catalogue" {
+  type              = "ingress"
+  security_group_id = local.mongodb_sg_id # 
+  source_security_group_id = local.catalogue_sg_id #
+  from_port         = 27017
+  protocol          = "tcp"
+  to_port           = 27017
+}
+
 #this is attached to bastion sg to allow ssh access from laptop to bastion host
 resource "aws_security_group_rule" "bastion_laptop" {
   type              = "ingress"
