a4a429e8037b6120b126800ed34bbe6e2f8cfbce
Shankar-codes <harikainteriors@gmail.com>
2026-05-19 17:47:27 +0530

    catalogue

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -82,7 +82,7 @@ resource "aws_lb_target_group" "catalogue" {
 # creating launch template for the catalogue instance
 resource "aws_launch_template" "catalogue" {
   name = "${local.common_name_suffix}-catalogue"
-  image_id = "aws_ami_from_instance.catalogue.id"
+  image_id = aws_ami_from_instance.catalogue.id
   instance_initiated_shutdown_behavior = "terminate"
   instance_type = "t3.micro"
 
