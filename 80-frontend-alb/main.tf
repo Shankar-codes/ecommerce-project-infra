2bcffdc939225916b4bbfdd1713eb693aec737b9
Shankar-codes <harikainteriors@gmail.com>
2026-05-25 16:21:14 +0530

    frontend load balancer

--- 80-frontend-alb/main.tf
+++ 80-frontend-alb/main.tf
@@ -1,5 +1,5 @@
 resource "aws_lb" "frontend_alb" {
-  name               = "${local.common_name_suffix}-frontend-alb"
+  name               = "${var.project_name}-frontend-alb"
   internal           = false
   load_balancer_type = "application"
   security_groups    = [local.frontend_alb_sg_id]
@@ -17,7 +17,7 @@ resource "aws_lb_listener" "frontend_alb" {
   load_balancer_arn = aws_lb.frontend_alb.arn
   port              = "443"
   protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-TLS13-1-3-2021-0"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
   certificate_arn   = local.frontend_alb_certificate_arn
 
     default_action {
