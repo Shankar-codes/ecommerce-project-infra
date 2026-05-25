761ac521f811c5afded5bd300f6b7a27f42e7bea
Shankar-codes <harikainteriors@gmail.com>
2026-05-25 19:09:39 +0530

    components

---
+++ 90-components/main.tf
@@ -0,0 +1,6 @@
+module "components" {
+  for_each = var.components
+  source = "git::https://github.com/Shankar-codes/terraform-ecommerce-component-module.git?ref=main"
+  component=each.key
+  rule_priority=each.value.rule_priority
+}
