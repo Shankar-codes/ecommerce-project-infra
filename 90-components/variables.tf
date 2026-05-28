0180a6ac9ef83e457b97722720e0232e543297a6
Shankar-codes <harikainteriors@gmail.com>
2026-05-28 12:38:36 +0530

    components

--- 90-components/variables.tf
+++ 90-components/variables.tf
@@ -17,7 +17,7 @@ variable "components" {
         cart = {
             rule_priority = 30
         }
-        shipping = {
+        Shipping = {
             rule_priority = 40
         }
         payment = {
