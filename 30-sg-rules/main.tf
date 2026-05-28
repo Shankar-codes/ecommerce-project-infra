74cbe3c054a4584b1c99b5f1410015c2f2cdb13c
Shankar-codes <harikainteriors@gmail.com>
2026-05-28 11:54:16 +0530

    security group rules

--- 30-sg-rules/main.tf
+++ 30-sg-rules/main.tf
@@ -309,7 +309,7 @@ resource "aws_security_group_rule" "backend_alb_payment" {
 # frontend alb connecting to frontend alb server on port 80
 resource "aws_security_group_rule" "frontend_frontend_alb" {
   type              = "ingress"
-  security_group_id = local.frontend_alb_sg_id # 
+  security_group_id = local.frontend_sg_id # 
   source_security_group_id = local.frontend_alb_sg_id #
   from_port         = 80
   protocol          = "tcp"
