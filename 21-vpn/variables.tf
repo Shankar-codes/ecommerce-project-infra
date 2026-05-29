6aa693db4c4006448f86f05de73f39cc38622e5f
Shankar-codes <harikainteriors@gmail.com>
2026-05-29 15:46:30 +0530

    openvpn

--- 21-vpn/variables.tf
+++ 21-vpn/variables.tf
@@ -6,10 +6,14 @@ variable "environment" {
   default = "dev"
 }
 
-variable "ami_id" {
-  default = "ami-0220d79f3f480ecf5"
-}
-
 variable "instance_type" {
   default = "t3.micro"
+}
+
+variable "zone_id" {
+  default = "Z00405822WO8HCQACP58A"
+}
+
+variable "domain_name" {
+  default     = "ellamma.fun"
 }
