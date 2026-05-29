403dd4a6f9143c1c426f601071dcc1f14b4d9e75
Shankar-codes <harikainteriors@gmail.com>
2026-05-29 15:02:57 +0530

    openvpn

--- 21-vpn/main.tf
+++ 21-vpn/main.tf
@@ -12,3 +12,10 @@ resource "aws_instance" "open_vpn" {
   )
 }  
 
+resource "aws_route53_record" "openvpn" {
+  zone_id = var.zone_id
+  name    = "openvpn.${var.domain_name}" #openvpn.ellamma.fun
+  type    = "A"
+  ttl     = 1
+  records = [aws_instance.open_vpn.public_ip]
+}
