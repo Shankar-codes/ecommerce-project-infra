6d1162569e9c50704763c0b0bcf22aed02b59496
Shankar-codes <harikainteriors@gmail.com>
2026-06-02 13:34:38 +0530

    security group rules

--- 30-sg-rules/main.tf
+++ 30-sg-rules/main.tf
@@ -353,7 +353,6 @@ resource "aws_security_group_rule" "components_vpn" {
   type              = "ingress"
   security_group_id = each.value.sg_id
   source_security_group_id = local.open_vpn_sg_id
-  cidr_blocks = ["0.0.0.0/0"]
   from_port         = each.value.port
   protocol          = "tcp"
   to_port           = each.value.port
