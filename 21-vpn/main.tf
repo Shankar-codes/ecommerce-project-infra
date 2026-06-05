8bfe827df0491d7d97d51256903ddf14ea4969ca
Shankar-codes <harikainteriors@gmail.com>
2026-06-05 11:46:22 +0530

    vpn

--- 21-vpn/main.tf
+++ 21-vpn/main.tf
@@ -3,8 +3,8 @@ resource "aws_instance" "open_vpn" {
   instance_type = var.instance_type
   vpc_security_group_ids = [local.open_vpn_sg_id]
   subnet_id = local.public_subnet_ids
-
   user_data = file("openvpn.sh")
+  
   tags =merge(local.common_tags, {
       Name = "${var.project_name}-${var.environment}-openvpn"
       Terraform = "true"
